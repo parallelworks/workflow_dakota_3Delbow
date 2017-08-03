ba7415bbb46360b776a5c6449d88276cda1668dd
Ubuntu <mattshax@gmail.com>
2017-08-03 13:31:35 -0400

    update surrogate model to cog free

--- dakota/start_docker_soga.sh
+++ dakota/start_docker_soga.sh
@@ -1,1 +1,1 @@
-docker run -i --rm -v $PWD:/scratch -w /scratch parallelworks/dakota_openfoam /bin/bash templatedir/runDakota.sh templatedir/dakota_DOE.in results/out.dat results/out.html
+docker run -i --rm -v $PWD:/scratch -v /core/xinchen/swift-k:/swift-k -w /scratch parallelworks/dakota_openfoam /bin/bash templatedir/runDakota.sh templatedir/dakota_DOE.in results/out.dat results/out.html
--- dakota/start_docker_surr.sh
+++ dakota/start_docker_surr.sh
@@ -1,1 +1,1 @@
-docker run -i --rm -v $PWD:/scratch -w /scratch parallelworks/dakota_openfoam /bin/bash templatedir/runDakota_surr.sh templatedir/dakota_surr.in results/surr.dat results/surr.html
+docker run -i --rm -v $PWD:/scratch -v /core/xinchen/swift-k:/swift-k -w /scratch parallelworks/dakota_openfoam /bin/bash templatedir/runDakota_surr.sh templatedir/dakota_surr.in results/surr.dat results/surr.html
